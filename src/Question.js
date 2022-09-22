8483adb2bd3489c4497379d59435630aec303dfa
MadinaSharapova97 <114133064+MadinaSharapova97@users.noreply.github.com>
2022-09-22 11:31:12 +0500

    Add files via upload

---
+++ src/Question.js
@@ -0,0 +1,54 @@
+export const Questions = [
+    {
+     
+      question:"O'zbekistonning poytaxti qaysi shahar ?",
+      answerList:[
+          {answer:"Toshkent", isCorrect:true},
+          {answer:"Samarqand", isCorrect:false},
+          {answer:"Buxoro", isCorrect:false},
+          {answer:"Farg'ona", isCorrect:false},
+      ]
+    },
+    {
+      
+      question:"Rossiyaning poytaxti qaysi shahar ?",
+      answerList:[
+          {answer:"London", isCorrect:false},
+          {answer:"New York", isCorrect:false},
+          {answer:"Jakarta", isCorrect:false},
+          {answer:"Moskva", isCorrect:true},
+      ]
+    },
+    {
+      
+      question:"Amerikaning  poytaxti qaysi shahar ?",
+      answerList:[
+          {answer:"Ottava", isCorrect:false},
+          {answer:"Vashington", isCorrect:true},
+          {answer:"London", isCorrect:false},
+          {answer:"Seul", isCorrect:false},
+      ]
+    },
+    {
+        
+        question:"Brazilianing  poytaxti qaysi shahar ?",
+        answerList:[
+            {answer:"Brazilia", isCorrect:true},
+            {answer:"Vashington", isCorrect:false},
+            {answer:"London", isCorrect:false},
+            {answer:"Seul", isCorrect:false},
+        ]
+      },
+      {
+        
+        question:"Britaniyaning  poytaxti qaysi shahar ?",
+        answerList:[
+            {answer:"Vashington", isCorrect:false},
+            {answer:"Seul", isCorrect:false},
+            {answer:"London", isCorrect:true},
+            {answer:"Brazilia", isCorrect:false},
+        ]
+      },
+  ]
+  
+  
