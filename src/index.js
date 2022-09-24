83606b9f839b43e580f5d7cc9f474814e701fd4f
MadinaSharapova97 <114133064+MadinaSharapova97@users.noreply.github.com>
2022-09-24 18:14:36 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,20 +9,14 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'react-toastify/dist/ReactToastify.css'
 import './style.css'
 import Context from './context/Context'
-// import  './quiz.css'
-// import {QuizApp} from './Quiz'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    
     <BrowserRouter>
         <ToastContainer />
         <Context>
             <App />
-        
         </Context>
-
     </BrowserRouter>
-
-    // <QuizApp/>
-
 );
